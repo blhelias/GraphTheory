c35bb286edd9255807418687d5dc6cc5b6443bbc
blhelias <brieuc.lhelias@gmail.com>
2018-11-18 22:35:17 +0100

    DEV - fix small issue -> previous node

--- Prim/prim.py
+++ Prim/prim.py
@@ -11,9 +11,6 @@ class Prim:
 
     def minimum_spanning_tree(self, source: int, node_map: Dict):
         pq = PriorityQueue()
-        # Initialize distance and visited parameter for each node
-        #for _, value_node in node_map.items():
-        #    pq.push(value_node)
         # Take the source node and push it int the pq
         source = node_map[source]
         source.dist = 0.
@@ -28,17 +25,21 @@ class Prim:
             # Check each neighbors
             for e in v.adj_list:
                 w = e.destination
+                print(w)
                 cost_vw = e.weight
                 # Check if weights are positives
                 if cost_vw < 0:
                     raise ValueError(" Graph has negative weight !!!")
                 # update distance of the node if necessary
-                if w.dist >= cost_vw:
+                if w.dist > cost_vw and not w.scratch: # Make sure the node 
+                # hasn't been discovered visited otherwise it will cause some issues.     
                     w.dist = cost_vw
                     w.prev = v
                     pq.push(Path(w, w.dist))
 
         for _, value_node  in node_map.items():
-            print(value_node)
+            rep = ""
+            rep = "{ node: " + str(value_node.id) + ", previous: "
             if value_node.prev is not None:
-                print(value_node.prev.id)
+                rep += str(value_node.prev.id) + " }"
+            print(rep)
